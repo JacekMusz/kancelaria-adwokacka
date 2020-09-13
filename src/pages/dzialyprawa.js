94ee78403fc33ba4e6dc2c6f6b7f9b1c7f7ac2a1
Jacek <muszynskijk@gmail.com>
2020-09-13 23:29:07 +0200

    syntax error fixed

--- src/pages/dzialyprawa.js
+++ src/pages/dzialyprawa.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import styled from "styled-components"
-import ListElement from "./../components/Elements/ListElement"
+import ListElement from "./../components/Elements/listElement"
 import StyledH3 from "../components/Elements/styledH3"
 
 const PageContainer = styled.div`
